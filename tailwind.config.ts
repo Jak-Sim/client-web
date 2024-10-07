691ecd46242523aabc39d0d22be10d4ff3002aa4
Dusuna <94776135+dusunax@users.noreply.github.com>
2024-10-08 02:33:31 +0900

    chore: tailwind.config에 컬러 시스템 적용

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -14,6 +14,28 @@ const config: Config = {
         primary: '#FF6700',
         secondary: '#FF8A3B',
         tertiary: '#FF9700',
+        surface: {
+          dark: '#1C1C1E',
+          light: '#F2F2F7',
+        },
+        text: {
+          dark: '#030303',
+          light: '#FFFFFF',
+        },
+        error: {
+          dark: '#EB4732',
+          light: '#EB4732',
+        },
+        submit: {
+          dark: '#30D158',
+          light: '#49C055',
+        },
+        grayscale: {
+          100: '#F6F6F6',
+          200: '#E2E2E2',
+          300: '#969696',
+          400: '#2E2E2E',
+        },
       },
     },
   },
