b343a8c5a867239861d1292d085bfeaa012b86e7
Dusuna <94776135+dusunax@users.noreply.github.com>
2024-10-14 18:36:38 +0900

    feat: axios 기본 설정

---
+++ src/app/api/axios.ts
@@ -0,0 +1,19 @@
+import axios, { type AxiosInstance, type AxiosRequestConfig, type AxiosResponse } from 'axios';
+import { API_URL } from '@/config/config';
+
+interface NextOptions {
+  tags?: string[];
+}
+
+interface CustomAxiosInstance extends AxiosInstance {
+  get: <T = unknown, R = AxiosResponse<T>, D = unknown>(
+    url: string,
+    config?: AxiosRequestConfig<D> & { next?: NextOptions },
+  ) => Promise<R>;
+}
+
+const api = axios.create({
+  baseURL: API_URL,
+});
+
+export default api as CustomAxiosInstance;
---
+++ src/config/config.ts
@@ -0,0 +1,4 @@
+const API_URL = 'http://localhost:3000'; // TODO: Change to backend URL environment variable
+const SOCKET_URL = 'http://localhost:3000'; // TODO: Change to backend URL environment variable
+
+export { API_URL, SOCKET_URL };
