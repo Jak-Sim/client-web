4a899020b506fa23ad25648859280dbbb7711554
Dusuna <94776135+dusunax@users.noreply.github.com>
2024-11-05 23:32:06 +0900

    feat: add priority to logo Image

--- src/app/(pages)/sign-in/page.tsx
+++ src/app/(pages)/sign-in/page.tsx
@@ -6,7 +6,7 @@ const Page = () => {
   return (
     <main className='w-full h-full flex flex-col bg-gradient-to-br from-[#FF825B] via-[#FA715E] to-[#F56060]'>
       <div className='flex-1 flex flex-col justify-center items-center'>
-        <Image src={logoLanding} alt='logo' className='w-[7.5rem]' />
+        <Image src={logoLanding} alt='logo' className='w-[7.5rem]' priority />
       </div>
 
       <div className='flex flex-col items-center justify-center gap-6 pb-16'>
