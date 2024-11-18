5312575417b99beff092c2739f5896923cf85eb2
Dusuna <94776135+dusunax@users.noreply.github.com>
2024-11-18 20:22:49 +0900

    feat: 챌린지 목록 카드 임시 레이아웃

--- src/app/(pages)/challenge/_components/ChallengeCard.tsx
+++ src/app/(pages)/challenge/_components/ChallengeCard.tsx
@@ -1,3 +1,15 @@
+import Image from "next/image";
+import challengeImage from "@/assets/images/placeholder/chat-list.png";
+
 export default function ChallengeCard(){
-  return <div className="bg-white rounded-lg p-4">ChallengeCard</div>;
+  return <div className="h-[200px] bg-white rounded-2xl relative overflow-hidden">
+    <Image src={challengeImage} alt="챌린지 이미지" fill className="object-cover" />
+  
+    <div className="absolute top-0 bottom-0 left-0 right-0 p-6">
+      <div className="text-white">
+        <h3 className="text-xl font-bold mb-1">챌린지 이름</h3>
+        <p className="text-sm">챌린지 설명</p>
+      </div>
+    </div>
+  </div>;
 };
