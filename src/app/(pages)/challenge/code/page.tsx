9646c39c58f45dadbd3c37e2d1d83176bbf024dd
Dusuna <94776135+dusunax@users.noreply.github.com>
2024-11-18 21:58:34 +0900

    feat: 챌린지 페이지에 레이아웃 리팩토링 반영

--- src/app/(pages)/challenge/code/page.tsx
+++ src/app/(pages)/challenge/code/page.tsx
@@ -1,11 +1,20 @@
+import Header from "@/components/layout/Header";
 import HeaderBackButton from "@/components/layout/HeaderBackButton";
 import PageLayout from "@/components/layout/PageLayout";
 import CodeForm from "./_components/CodeForm";
 
 const Page = () => {
   return (
-    <PageLayout header={{ title: "코드 찾아보기", left: <HeaderBackButton /> }} className="bg-v1-background">
-        <CodeForm />
+    <PageLayout
+      header={
+        <Header className="bg-v1-background border-none">
+          <Header.BackButton />
+          <Header.Center>코드 찾아보기</Header.Center>
+        </Header>
+      }
+      className="bg-v1-background"
+    >
+      <CodeForm />
     </PageLayout>
   );
 };
--- src/app/(pages)/challenge/none/page.tsx
+++ src/app/(pages)/challenge/none/page.tsx
@@ -1,10 +1,18 @@
 import HeaderBackButton from "@/components/layout/HeaderBackButton";
 import PageLayout from "@/components/layout/PageLayout";
 import ChallengeLinkButtonBox from "./_components/ChallengeLinkButtonBox";
+import Header from "@/components/layout/Header";
 
 const Page = () => {
   return (
-    <PageLayout header={{ title: "챌린지", left: <HeaderBackButton /> }} className="bg-v1-background">
+    <PageLayout
+      header={
+        <Header className="bg-v1-background border-none">
+          <Header.BoldText bold="챌린지" />
+        </Header>
+      }
+      className="bg-v1-background"
+    >
       <div className="flex flex-col h-full justify-between px-6 pb-16">
         <div className="flex-1 flex flex-col items-center justify-center">
           <p className="text-2xl text-v1-text-primary-300 break-keep text-center">아직 시작한 챌린지가 없어요</p>
