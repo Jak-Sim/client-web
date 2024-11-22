4651d47f6eac4fa36111a9b7f401f09eddfacd42
catbell_ted <changsoonyun@naver.com>
2024-11-22 14:42:03 +0900

    feat: 프레이머 모션 라이브러리 추가 및 적용

--- src/app/(pages)/feed/[userId]/page.tsx
+++ src/app/(pages)/feed/[userId]/page.tsx
@@ -1,5 +1,8 @@
 'use client';
 
+import { useRef } from 'react';
+import { motion } from 'framer-motion';
+import { useClickAway } from 'react-use';
 import FeedContents from '@/app/(pages)/feed/[userId]/_components/FeedContents';
 import FeedUserProfile from '@/app/(pages)/feed/[userId]/_components/FeedUserProfile';
 import Dots from '@/assets/images/icons/Dots.svg';
@@ -11,6 +14,12 @@ import { useModal } from '@/hooks/useModal';
 
 const Page = () => {
   const modalProps = useModal('feed-profile');
+  const modalRef = useRef<HTMLDivElement>(null);
+
+  useClickAway(modalRef, () => {
+    modalProps.closeModal();
+  });
+
   return (
     <PageLayout
       className={'bg-v1-background'}
@@ -24,39 +33,39 @@ const Page = () => {
             </div>
           </Header.Item>
           <Header.Item>
-            <Header.Icon Icon={Dots} onClick={modalProps.toggleModal} />
+            <Header.Icon Icon={Dots} onClick={modalProps.openModal} />
           </Header.Item>
         </Header>
       }
     >
       <FeedUserProfile />
       <FeedContents />
-      {modalProps.isOpen && (
-        <Portal>
-          <Modal modalProps={modalProps}>
-            <div className={'flex h-full items-end'}>
-              <div className={'m-6 flex-1 bg-white p-6'}>
-                <h2 className='mb-4 text-center text-lg font-bold'>모달 제목</h2>
-                <p className='mb-6 text-center text-gray-700'>여기에 내용을 입력하세요.</p>
-                <div className='flex justify-between gap-2'>
-                  <button
-                    onClick={modalProps.toggleModal}
-                    className='flex-1 rounded-md bg-gray-200 py-2 text-gray-800 shadow transition hover:bg-gray-300'
-                  >
-                    닫기
-                  </button>
-                  <button className='flex-1 rounded-md bg-yellow-500 py-2 text-white shadow transition hover:bg-yellow-600'>
-                    차단하기
-                  </button>
-                  <button className='flex-1 rounded-md bg-red-500 py-2 text-white shadow transition hover:bg-red-600'>
-                    신고하기
-                  </button>
-                </div>
+      <Portal>
+        <Modal modalProps={modalProps}>
+          <motion.div
+            className={'flex h-full items-end'}
+            initial={{ y: 100, opacity: 0 }}
+            animate={{ y: 0, opacity: 1 }}
+            exit={{ y: 100, opacity: 0 }}
+            transition={{ duration: 0.3 }}
+          >
+            <div className={'mx-6 mb-[44px] flex-1'} ref={modalRef}>
+              <div className={'mb-[6px] flex flex-col rounded-2xl bg-white'}>
+                <button className={'w-full border-b border-v1-grey-400 p-5 text-xl font-medium text-[#ff3b30]'}>
+                  차단하기
+                </button>
+                <button className={'w-full p-5 text-xl font-medium text-v1-grey-900'}>신고하기</button>
               </div>
+              <button
+                onClick={modalProps.closeModal}
+                className={'w-full rounded-2xl bg-white p-5 text-xl font-medium text-v1-grey-900'}
+              >
+                취소
+              </button>
             </div>
-          </Modal>
-        </Portal>
-      )}
+          </motion.div>
+        </Modal>
+      </Portal>
     </PageLayout>
   );
 };
--- src/components/modal/Modal.tsx
+++ src/components/modal/Modal.tsx
@@ -1,4 +1,7 @@
+'use client';
+
 import { ReactNode } from 'react';
+import { AnimatePresence, motion } from 'framer-motion';
 import { ModalProps } from '@/hooks/useModal';
 import { cn } from '@/lib/shadcn/utils';
 
@@ -12,15 +15,22 @@ const Modal = ({
   children: ReactNode;
 }) => {
   return (
-    <div
-      className={cn(
-        'fixed inset-0 m-auto h-screen w-[400px]',
-        hasBackDrop && 'bg-black bg-opacity-20 backdrop-blur-sm',
+    <AnimatePresence>
+      {modalProps.isOpen && (
+        <motion.div
+          className={cn(
+            'fixed inset-0 m-auto h-screen w-[400px]',
+            hasBackDrop && 'bg-black bg-opacity-20 backdrop-blur-sm',
+          )}
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          exit={{ opacity: 0 }}
+          transition={{ duration: 0.3 }}
+        >
+          {children}
+        </motion.div>
       )}
-      onClick={modalProps.closeModal}
-    >
-      {children}
-    </div>
+    </AnimatePresence>
   );
 };
 
