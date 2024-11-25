56a1ed7b04e89465c70711eaed5183d9710a2a25
Dusuna <94776135+dusunax@users.noreply.github.com>
2024-11-25 17:11:03 +0900

    feat: Slider 로직을 컴포넌트 내부로 이동

--- src/app/(pages)/challenge/create/_components/TwoWaySlider.tsx
+++ src/app/(pages)/challenge/create/_components/TwoWaySlider.tsx
@@ -1,5 +1,7 @@
-import { useEffect, useState } from 'react';
 import Slider from '@/components/input/Slider';
+
+const MIN = 2;
+const MAX = 10;
 
 export default function TwoWaySlider({
   value,
@@ -8,62 +10,15 @@ export default function TwoWaySlider({
   value: [number, number];
   onChange: (value: [number, number]) => void;
 }) {
-  const defaultTwoWayValue: [number, number] = [value[0], value[1]];
-  const min = 2;
-  const max = 10;
-
-  const getSliderValue = (value: number) => {
-    return ((value - min) / (max - min)) * 100;
-  };
-
-  const [inputValue, setInputValue] = useState<[number, number]>(defaultTwoWayValue);
-  const [sliderValue, setSliderValue] = useState<[number, number]>([
-    getSliderValue(defaultTwoWayValue[0]),
-    getSliderValue(defaultTwoWayValue[1]),
-  ]);
-
-  const handleTwoWaySliderChange = (event: React.ChangeEvent<HTMLInputElement>, type: 'from' | 'to') => {
-    const value = parseInt(event.target.value);
-
-    if (type === 'from') {
-      if (value <= inputValue[1]) {
-        setSliderValue([getSliderValue(value), sliderValue[1]]);
-        setInputValue([value, inputValue[1]]);
-        onChange([value, inputValue[1]]);
-      } else {
-        event.target.value = inputValue[1].toString();
-      }
-    } else {
-      if (value >= inputValue[0]) {
-        setSliderValue([sliderValue[0], getSliderValue(value)]);
-        setInputValue([inputValue[0], value]);
-        onChange([inputValue[0], value]);
-      } else {
-        event.target.value = inputValue[0].toString();
-      }
-    }
-  };
-
-  useEffect(() => {
-    setInputValue(value);
-    setSliderValue([getSliderValue(value[0]), getSliderValue(value[1])]);
-  }, [value]);
-
   return (
-    <Slider>
+    <Slider defaultValue={value} max={MAX} min={MIN} onChange={onChange}>
       <Slider.TipWrapper>
-        <Slider.Tip left={sliderValue[0]} label='최소' value={inputValue[0]} />
-        <Slider.Tip left={sliderValue[1]} label='최대' value={inputValue[1]} />
+        <Slider.FromTip label='최소' />
+        <Slider.ToTip label='최대' />
       </Slider.TipWrapper>
 
-      <Slider.TwoWaySlider
-        min={min}
-        max={max}
-        defaultValue={defaultTwoWayValue}
-        sliderValue={sliderValue}
-        onChange={handleTwoWaySliderChange}
-      />
-      <Slider.Indicator length={max - min + 1} />
+      <Slider.TwoWaySlider />
+      <Slider.Indicator />
     </Slider>
   );
 }
--- src/components/input/Slider.tsx
+++ src/components/input/Slider.tsx
@@ -1,67 +1,113 @@
-import { ReactNode } from 'react';
+import { createContext, ReactNode, useContext, useEffect, useState } from 'react';
 import { cn } from '@/lib/shadcn/utils';
 
 
 interface SliderProps {
+  min: number;
+  max: number;
+  defaultValue: [number, number];
+  onChange: (value: [number, number]) => void;
   children: ReactNode;
 }
 
-const SliderWrapper = ({ children }: SliderProps) => {
-  return <div className='relative'>{children}</div>;
+interface SliderContextProps extends Omit<SliderProps, 'children'> {
+  handleSliderChange: (event: React.ChangeEvent<HTMLInputElement>, type: 'from' | 'to') => void;
+  sliderPercentage: [number, number];
+  inputValue: [number, number];
+}
+
+const SliderContext = createContext<SliderContextProps | undefined>(undefined);
+
+const getSliderValue = (value: number, min: number, max: number) => {
+  return ((value - min) / (max - min)) * 100;
 };
 
-const OneWaySlider = ({
-  min,
-  max,
-  defaultValue,
-  sliderValue,
-  onChange,
-}: {
-  min: number;
-  max: number;
-  defaultValue: number;
-  sliderValue: [number, number];
-  onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
-}) => {
+const SliderWrapper = ({ children, ...props }: SliderProps) => {
+  const { min, max, defaultValue, onChange } = props;
+  const [inputValue, setInputValue] = useState<[number, number]>(defaultValue as [number, number]);
+  const [sliderPercentage, setSliderPercentage] = useState<[number, number]>([
+    getSliderValue((defaultValue as [number, number])[0], min, max),
+    getSliderValue((defaultValue as [number, number])[1], min, max),
+  ]);
+
+  const handleSliderChange = (event: React.ChangeEvent<HTMLInputElement>, type: 'from' | 'to') => {
+    const value = parseInt(event.target.value);
+
+    if (type === 'from') {
+      if (value <= inputValue[1]) {
+        setSliderPercentage([getSliderValue(value, min, max), sliderPercentage[1]]);
+        setInputValue([value, inputValue[1]]);
+        onChange([value, inputValue[1]]);
+      } else {
+        event.target.value = inputValue[1].toString();
+      }
+    } else {
+      if (value >= inputValue[0]) {
+        setSliderPercentage([sliderPercentage[0], getSliderValue(value, min, max)]);
+        setInputValue([inputValue[0], value]);
+        onChange([inputValue[0], value]);
+      } else {
+        event.target.value = inputValue[0].toString();
+      }
+    }
+  };
+
+  useEffect(() => {
+    setSliderPercentage([
+      getSliderValue((defaultValue as [number, number])[0], min, max),
+      getSliderValue((defaultValue as [number, number])[1], min, max),
+    ]);
+  }, [defaultValue]);
+
+  return (
+    <SliderContext.Provider value={{ ...props, sliderPercentage, handleSliderChange, inputValue }}>
+      {children}
+    </SliderContext.Provider>
+  );
+};
+
+const OneWaySlider = () => {
+  const { min, max, defaultValue, sliderPercentage, handleSliderChange } = useContext(SliderContext)!;
+
   return (
     <div className='relative mb-5 mt-4 h-[6px]'>
       <SliderTrack>
-        <SliderProgress style={{ left: `${sliderValue[0]}%`, right: `${100 - sliderValue[1]}%` }} />
+        <SliderProgress style={{ left: 0, right: `${100 - sliderPercentage[1]}%` }} />
       </SliderTrack>
 
       <div className='absolute top-1/2 -mt-[2px] w-full -translate-y-1/2'>
-        <SliderInput min={min} max={max} onChange={onChange} defaultValue={defaultValue} />
+        <SliderInput
+          min={min}
+          max={max}
+          onChange={(event) => handleSliderChange(event, 'to')}
+          defaultValue={(defaultValue as [number, number])[1]}
+        />
       </div>
     </div>
   );
 };
 
-const TwoWaySlider = ({
-  min,
-  max,
-  defaultValue,
-  sliderValue,
-  onChange,
-}: {
-  min: number;
-  max: number;
-  defaultValue: [number, number];
-  sliderValue: [number, number];
-  onChange: (event: React.ChangeEvent<HTMLInputElement>, type: 'from' | 'to') => void;
-}) => {
+const TwoWaySlider = () => {
+  const { min, max, defaultValue, sliderPercentage, handleSliderChange } = useContext(SliderContext)!;
+
   return (
     <div className='relative my-4 w-full'>
       <SliderTrack>
-        <SliderProgress style={{ left: `${sliderValue[0]}%`, right: `${100 - sliderValue[1]}%` }} />
+        <SliderProgress style={{ left: `${sliderPercentage[0]}%`, right: `${100 - sliderPercentage[1]}%` }} />
       </SliderTrack>
 
       <div className='absolute top-1/2 -mt-[2px] w-full -translate-y-[calc(50%-12px)]'>
-        <SliderInput min={min} max={max} onChange={(event) => onChange(event, 'from')} defaultValue={defaultValue[0]} />
         <SliderInput
           min={min}
           max={max}
-          onChange={(event) => onChange(event, 'to')}
-          defaultValue={defaultValue[1]}
+          onChange={(event) => handleSliderChange(event, 'from')}
+          defaultValue={(defaultValue as [number, number])[0]}
+        />
+        <SliderInput
+          min={min}
+          max={max}
+          onChange={(event) => handleSliderChange(event, 'to')}
+          defaultValue={(defaultValue as [number, number])[1]}
           className='-translate-y-6'
         />
       </div>
@@ -106,23 +152,36 @@ const SliderInput = ({
   );
 };
 
-const SliderIndicator = ({ length }: { length: number }) => {
+const SliderIndicator = () => {
+  const { min, max } = useContext(SliderContext)!;
+  const indicatorLength = max - min + 1;
+
   return (
     <div className='flex h-[6px] justify-between px-2'>
-      {Array.from({ length }).map((_, index) => (
+      {Array.from({ length: indicatorLength }).map((_, index) => (
         <div key={index} className='h-full w-[2px] rounded-full bg-v1-grey-400' />
       ))}
     </div>
   );
 };
 
-const SliderTip = ({ left, label, value }: { left: number; label: string; value: number }) => {
+const FromSliderTip = ({ label }: { label: string }) => {
+  return <SliderTip label={label} type='from' />;
+};
+
+const ToSliderTip = ({ label }: { label: string }) => {
+  return <SliderTip label={label} type='to' />;
+};
+
+const SliderTip = ({ label, type }: { label: string; type: 'from' | 'to' }) => {
+  const { sliderPercentage, inputValue } = useContext(SliderContext)!;
+
   return (
     <div className='absolute left-0 mb-4 w-full'>
-      <div className='absolute w-10 -translate-x-1/2' style={{ left: `${left}%` }}>
+      <div className='absolute w-10 -translate-x-1/2' style={{ left: `${sliderPercentage[type === 'from' ? 0 : 1]}%` }}>
         <p className='mb-1 bg-v1-background text-center text-xs text-v1-text-primary-400'>{label}</p>
         <div className='flex h-[30px] w-10 items-center justify-center rounded-lg bg-v1-orange-500 text-sm font-semibold text-white'>
-          {value}
+          {type === 'from' ? inputValue[0] : inputValue[1]}
         </div>
       </div>
     </div>
@@ -133,14 +192,16 @@ const SliderTipWrapper = ({ children }: { children: ReactNode }) => {
   return <div className='relative mx-[10px] h-[50px]'>{children}</div>;
 };
 
-function Slider({ children }: { children: ReactNode }) {
-  return <SliderWrapper>{children}</SliderWrapper>;
+function Slider({ children, ...props }: SliderProps) {
+  return <SliderWrapper {...props}>{children}</SliderWrapper>;
 }
 
 Slider.OneWaySlider = OneWaySlider;
 Slider.TwoWaySlider = TwoWaySlider;
 Slider.Indicator = SliderIndicator;
-Slider.Tip = SliderTip;
 Slider.TipWrapper = SliderTipWrapper;
+Slider.Tip = SliderTip;
+Slider.FromTip = FromSliderTip;
+Slider.ToTip = ToSliderTip;
 
 export default Slider;
