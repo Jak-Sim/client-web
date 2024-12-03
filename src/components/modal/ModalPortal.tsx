54fd518449da5f408a130e55b916d50b46a4e6d7
catbell_ted <changsoonyun@naver.com>
2024-12-03 23:36:56 +0900

    fix: modalRoot type assertion 수정

--- src/components/modal/ModalPortal.tsx
+++ src/components/modal/ModalPortal.tsx
@@ -1,16 +1,15 @@
-import React, { ReactElement, useEffect, useState } from 'react';
+import { ReactElement, useEffect, useState } from 'react';
 import { createPortal } from 'react-dom';
 
 const Portal = ({ children }: { children: ReactElement }) => {
   const [mounted, setMounted] = useState<boolean>(false);
-  const modalRoot = document.getElementById('modal-root')!;
-  console.assert(modalRoot, 'modal-root not found');
+  const modalRoot = document.getElementById('modal-root');
 
   useEffect(() => {
     setMounted(true);
   }, []);
 
-  if (!mounted) return <></>;
+  if (!mounted || !modalRoot) return null;
   return createPortal(children, modalRoot);
 };
 
