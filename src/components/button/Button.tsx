5693c88608d331ba34c56f6a79185464c8395e61
Dusuna 두선아 <94776135+dusunax@users.noreply.github.com>
2024-12-08 17:48:07 +0900

    feat: 챌린지 코드 검색 기능 (#9)
    * feat: 아이콘 추가, 버튼 텍스트 변경
    
    * feat: Button 컴포넌트에 스타일 가이드 반영
    
    * feat: api 요청 주석 작성
    
    * feat: 버튼 props 오류 수정
    
    * feat: 챌린지 코드 참여 성공 페이지 삭제
    
    * feat: 챌린지 리디렉션 수정
    
    * feat: 버튼 로딩 아이콘에 currentColor 적용

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -1,16 +1,22 @@
 import { ButtonHTMLAttributes } from 'react';
 import LoadingIcon from '@/assets/images/icons/loading.svg';
 
+export type ButtonVariant = 'primary' | 'secondary' | 'tertiary';
+
 interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   isLoading?: boolean;
-  variant?: 'primary' | 'outline';
+  variant?: ButtonVariant;
 }
 
-export default function Button({ children, className, isLoading, variant, ...props }: ButtonProps) {
-  const vaiantStyle = variant==='outline' ? '!bg-white !text-v1-primary border-[2px] !border-v1-primary' : '';
+export default function Button({ children, className, isLoading, variant = 'primary', ...props }: ButtonProps) {
+  const variantStyle =
+    variant === 'secondary' ? 'button-secondary' : variant === 'tertiary' ? 'button-tertiary' : 'button-primary';
 
   return (
-    <button className={`button button-primary min-h-[3.75rem] w-full rounded-[20px] text-xl font-bold ${className} ${vaiantStyle} shadow-sm`} {...props}>
+    <button
+      className={`button min-h-[3.75rem] w-full rounded-[20px] text-xl font-bold ${className} shadow-sm ${variantStyle}`}
+      {...props}
+    >
       <div className='flex items-center justify-center gap-1'>
         {isLoading ? (
           <LoadingIcon alt='loading' className='h-6 w-6 animate-spin' style={{ animationDuration: '3s' }} />
