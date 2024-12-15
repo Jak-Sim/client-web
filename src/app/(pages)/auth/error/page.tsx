4d5ebda9512ebc41de8af03ae174d7248f3978cf
Dusuna <94776135+dusunax@users.noreply.github.com>
2024-12-15 22:43:04 +0900

    feat: 페이지 내 에러 메시지 출력

--- src/app/(pages)/auth/error/page.tsx
+++ src/app/(pages)/auth/error/page.tsx
@@ -1,10 +1,13 @@
 import Link from 'next/link';
 import Button from '@/components/button/Button';
 
-export default function Page() {
+export default function Page({ searchParams }: { searchParams: { message: string } }) {
+  const message = searchParams.message;
+
   return (
     <main className='flex h-full w-full flex-col items-center justify-center gap-12 bg-v1-background px-6'>
       <h2 className='text-xl font-semibold text-v1-text-primary-500'>소셜 로그인에 실패했습니다.</h2>
+      {message && <p className='text-center text-sm text-v1-text-primary-200'>{message}</p>}
       <Link href='/auth/sign-in' className='w-full'>
         <Button variant='secondary'>돌아가기</Button>
       </Link>
