c65264e9119e1fb0a847633943582a0a3d939a65
Dusuna <94776135+dusunax@users.noreply.github.com>
2024-12-17 01:04:36 +0900

    feat: 미션 아이템 컴포넌트 분리, challenge/detail 내 미션 리스트

--- src/app/(pages)/challenge/_components/RewardList.tsx
+++ src/app/(pages)/challenge/_components/RewardList.tsx
@@ -3,7 +3,7 @@ import RewardItem from './RewardItem';
 
 export default function RewardList({ rewards }: { rewards: Reward[] }) {
   return (
-    <div className='-mt-4 flex flex-col gap-2 pb-6'>
+    <ul className='-mt-4 flex flex-col gap-2 pb-6'>
       {rewards.map((reward, index) => (
         <RewardItem
           key={reward.id}
@@ -11,6 +11,6 @@ export default function RewardList({ rewards }: { rewards: Reward[] }) {
           className={`mx-6 rounded-none border-b !px-0 ${index === rewards.length - 1 ? '' : 'border-b'}`}
         />
       ))}
-    </div>
+    </ul>
   );
 }
