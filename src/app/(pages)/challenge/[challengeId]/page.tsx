646811f2d322ea94b4b004125e70278263eaa72d
Dusuna <94776135+dusunax@users.noreply.github.com>
2024-12-17 00:50:19 +0900

    feat: 리워드 아이템 컴포넌트 분리, challenge/detail 내 리워드 리스트

--- src/app/(pages)/challenge/[challengeId]/page.tsx
+++ src/app/(pages)/challenge/[challengeId]/page.tsx
@@ -6,9 +6,13 @@ import { Hamburger, Speaker } from '@/assets/images/icons';
 import Header from '@/components/layout/Header';
 import PageLayout from '@/components/layout/PageLayout';
 import { DummyChallenge } from '../_components/ChallengeList';
+import RewardItem from '../_components/RewardItem';
+import dummyMission from '../_mock/dummyMission.json';
+import dummyReward from '../_mock/dummyReward.json';
+import AddItemButton from './_components/AddItemButton';
 import UserChallengeCard from './_components/UserChallengeCard';
 
-const Page = ({ params }: { params: { challengeId: string } }) => {
+export default function Page({ params }: { params: { challengeId: string } }) {
   const challengeId = params.challengeId;
   const challenge = DummyChallenge;
   const session = useSession();
@@ -17,6 +21,8 @@ const Page = ({ params }: { params: { challengeId: string } }) => {
   console.log('fetch challenge data with id: ' + challengeId);
   console.log('user have to join challenge: ', session.data?.user);
 
+  const rewards = dummyReward;
+  
   return (
     <PageLayout
       header={
@@ -33,11 +39,15 @@ const Page = ({ params }: { params: { challengeId: string } }) => {
           </div>
         </Header>
       }
-      className='bg-v1-background px-6'
+      className='bg-v1-background px-6 pt-2'
     >
       <UserChallengeCard challenge={challenge} />
+      <div className='flex flex-col gap-4 pt-3'>
+        {rewards.map((reward) => (
+          <RewardItem key={reward.id} reward={reward} hasFavorite={true} />
+        ))}
+        <AddItemButton color='blue' onClick={() => {}} />
+      </div>
     </PageLayout>
   );
-};
-
-export default Page;
+}
