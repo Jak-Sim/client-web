3fbd020893c91e0a2e1d513f5fc29539eccbeb4d
Dusuna <94776135+dusunax@users.noreply.github.com>
2024-12-20 20:57:54 +0900

    feat: 버튼 UI 업데이트

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -1,13 +1,26 @@
 import { ButtonHTMLAttributes } from 'react';
 import LoadingIcon from '@/assets/images/icons/loading.svg';
 
-
-export type ButtonVariant = 'primary' | 'secondary' | 'tertiary';
+const BUTTON_SIZE_STYLE = {
+  sm: '!min-h-[2.25rem] text-base rounded-[20px]',
+  md: '!min-h-[2.75rem] text-base rounded-2xl',
+  lg: '!min-h-[3.75rem] text-xl rounded-[20px]',
+};
+const BUTTON_VARIANT_STYLE = {
+  primary: 'button-primary',
+  secondary: 'button-secondary',
+  tertiary: 'button-tertiary',
+  outline: 'button-outline',
+  blue: 'button-blue',
+};
 
 interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement | HTMLDivElement> {
   isLoading?: boolean;
-  variant?: ButtonVariant;
+  variant?: keyof typeof BUTTON_VARIANT_STYLE;
   asDiv?: boolean;
+  className?: string;
+  size?: keyof typeof BUTTON_SIZE_STYLE;
+  icon?: React.ReactNode;
 }
 
 export default function Button({
@@ -16,16 +29,16 @@ export default function Button({
   isLoading,
   variant = 'primary',
   asDiv: asChild,
+  size = 'lg',
+  icon,
   ...props
 }: ButtonProps) {
-  const variantStyle =
-    variant === 'secondary' ? 'button-secondary' : variant === 'tertiary' ? 'button-tertiary' : 'button-primary';
+  const variantStyle = BUTTON_VARIANT_STYLE[variant];
 
   const Component = asChild ? 'div' : 'button';
-
   return (
     <Component
-      className={`button min-h-[3.75rem] w-full rounded-[20px] text-xl font-bold ${className} flex items-stretch shadow-sm ${variantStyle}`}
+      className={`button w-full font-bold ${className} flex items-stretch shadow-sm ${variantStyle} ${BUTTON_SIZE_STYLE[size]}`}
       {...props}
     >
       <div className='flex flex-1 items-center justify-center gap-1'>
@@ -33,6 +46,7 @@ export default function Button({
           <LoadingIcon alt='loading' className='h-6 w-6 animate-spin' style={{ animationDuration: '3s' }} />
         ) : null}
         {children}
+        {icon}
       </div>
     </Component>
   );
