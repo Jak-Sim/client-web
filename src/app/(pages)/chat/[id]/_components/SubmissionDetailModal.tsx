025acb587da93d098786bc88d1915e169bcbcb49
catbell_ted <changsoonyun@naver.com>
2025-02-26 21:03:57 +0900

    style: css 수정

--- src/app/(pages)/chat/[id]/_components/SubmissionDetailModal.tsx
+++ src/app/(pages)/chat/[id]/_components/SubmissionDetailModal.tsx
@@ -21,11 +21,12 @@ const SubmissionDetailModal = (props: SubMissionImageProps & ChatSubmissionProps
         }
         footer={<Fragment />}
       >
-        <div className={'relative h-full flex-1 px-4 py-2'}>
-          <p className={'pb-3'}>
+        <div className={'relative h-full flex-1 py-2'}>
+          <p className={'flex items-center gap-1 px-4 pb-3 text-xl font-medium text-v1-text-primary-400'}>
             <LightningDark />
+            {props.title}
           </p>
-          <ul className='-ml-6 flex w-full gap-2 overflow-hidden px-6'>
+          <ul className='flex w-full gap-2 overflow-hidden px-4'>
             <li className={'w-full flex-none'}>
               <div className='relative aspect-square rounded-[20px] bg-gray-300'>
                 <Image src={props.src || defaultAvatar} alt={'image'} fill />
@@ -38,23 +39,25 @@ const SubmissionDetailModal = (props: SubMissionImageProps & ChatSubmissionProps
               <div className='aspect-square rounded-[20px] bg-gray-300'></div>
             </li>
           </ul>
-          <p className={'pt-4 text-v1-text-primary-400'}>미션후기</p>
-          <p className={'rounded-[20px] bg-white p-[14px] text-v1-text-primary-400'}>{props.description}</p>
-          <div className={'flex w-full justify-between pt-6'}>
-            <button
-              className='flex flex-1 items-center justify-center gap-2 rounded-[20px] bg-white p-4 text-xl font-semibold shadow'
-              type='button'
-            >
-              <ChatSubmissionCancel />
-              다시 도전
-            </button>
-            <button
-              className='flex flex-1 items-center justify-center gap-2 rounded-[20px] bg-white p-4 text-xl font-semibold shadow'
-              type='button'
-            >
-              <ChatSubmissionConfirm />
-              미션 확인
-            </button>
+          <div className={'px-4'}>
+            <p className={'pt-4 font-medium text-v1-text-primary-400'}>미션후기</p>
+            <p className={'rounded-[20px] bg-white p-[14px] text-v1-text-primary-400'}>{props.description}</p>
+            <div className={'flex w-full justify-between pt-6'}>
+              <button
+                className='flex flex-1 items-center justify-center gap-2 rounded-[20px] bg-white p-4 text-xl font-semibold shadow'
+                type='button'
+              >
+                <ChatSubmissionCancel />
+                다시 도전
+              </button>
+              <button
+                className='flex flex-1 items-center justify-center gap-2 rounded-[20px] bg-white p-4 text-xl font-semibold shadow'
+                type='button'
+              >
+                <ChatSubmissionConfirm />
+                미션 확인
+              </button>
+            </div>
           </div>
         </div>
       </PageLayout>
